ec8d6aa72ab032df7538c4ed80ef5166fcc96ec2
unknown <varunmalpani.vm01@gmail.com>
2023-05-12 10:59:35 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Chatpage from './components/Chatpage';
+import Test from './components/Test';
 
 function App() {
   return (
     <div className="App">
       <Route path="/" component={HomePage} exact />
       <Route path="/chats" component={Chatpage} />
+      <Route path="/tst" component={Test} />
+
     </div>
   );
 }
